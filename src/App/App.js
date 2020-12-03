ac0cd8bea0cf542eadfca411251d7da22d408eb3
josephtmartin <goldglovejo13@gmail.com>
2020-12-03 17:56:02 -0600

    setting up firebase user and navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import './App.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
+import fbConnection from '../helpers/data/connection';
 
-class App extends React.Component {
+import Navbar from '../components/Navbar';
+import Routes from '../helpers/Routes';
+
+fbConnection();
+export default class App extends React.Component {
+  state = {
+    user: null,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { user } = this.state;
+
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <Router>
+          <Navbar user={user}/>
+          <Routes user={user}/>
+        </Router>
       </div>
     );
   }
 }
-
-export default App;
