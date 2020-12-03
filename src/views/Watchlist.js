201ad20621e37fe632ce95f45e73bf2fde23bde8
josephtmartin <goldglovejo13@gmail.com>
2020-12-03 17:22:37 -0600

    added file structure

---
+++ src/views/Watchlist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Watchlist extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <h1>Watchlist Component</h1>
+    );
+  }
+}
