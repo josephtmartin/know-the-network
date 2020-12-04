9a651b4a691e8b405335e2109e0e1ca0b99fd40f
josephtmartin <goldglovejo13@gmail.com>
2020-12-03 19:16:00 -0600

    added the login functionality and stored the user in firebase

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import Auth from '../components/Auth';
 
 export default class Home extends Component {
   state = {
 
   }
 
+  loadSignIn = () => {
+    const { user } = this.props;
+    let component = '';
+    if (!user) {
+      component = <Auth/>;
+    }
+    return component;
+  }
+
   render() {
     return (
-      <h1>Home Component</h1>
+      <div>
+        <h1 className='d-flex justify-content-center mt-3'>Home Component</h1>
+        <div className='d-flex flex-wrap container justify-content-center'>
+          {this.loadSignIn()}
+        </div>
+      </div>
     );
   }
 }
