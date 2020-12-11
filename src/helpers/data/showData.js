086f9986ff328502477dd1160de0a1d70c4ee58e
josephtmartin <goldglovejo13@gmail.com>
2020-12-10 19:07:31 -0600

    saving progress and fixed apiKey issue

--- src/helpers/data/showData.js
+++ src/helpers/data/showData.js
@@ -1,43 +1,43 @@
 import axios from 'axios';
 
-// const baseUrl = 'https://www.episodate.com/api';
-const baseUrl = 'https://know-the-network-default-rtdb.firebaseio.com';
-
-// const getMostPopular = () => new Promise((resolve, reject) => {
-//   axios.get(`${baseUrl}/most-popular?page=1`)
-//     .then((response) => {
-//       resolve(response.data.tv_shows);
-//     }).catch((error) => reject(error));
-// });
-
-// const searchShows = (term) => new Promise((resolve, reject) => {
-//   axios.get(`${baseUrl}/search?q=${term}`)
-//     .then((response) => {
-//       resolve(response.data.tv_shows);
-//     }).catch((error) => reject(error));
-// });
-
-// const getSingleShow = (id) => new Promise((resolve, reject) => {
-//   axios.get(`${baseUrl}/show-details?q=${id}`)
-//     .then((response) => {
-//       resolve(response.data.tvShow);
-//     }).catch((error) => reject(error));
-// });
+const baseUrl = 'https://www.episodate.com/api';
+// const baseUrl = 'https://know-the-network-default-rtdb.firebaseio.com';
 
 const getMostPopular = () => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/shows.json`)
+  axios.get(`${baseUrl}/most-popular?page=1`)
     .then((response) => {
-      resolve(Object.values(response.data));
+      resolve(response.data.tv_shows);
+    }).catch((error) => reject(error));
+});
+
+const searchShows = (term) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/search?q=${term}`)
+    .then((response) => {
+      resolve(response.data.tv_shows);
     }).catch((error) => reject(error));
 });
 
 const getSingleShow = (id) => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/shows.json?orderBy="id"&equalTo=${id}`)
+  axios.get(`${baseUrl}/show-details?q=${id}`)
     .then((response) => {
-      resolve(Object.values(response.data)[0]);
+      resolve(response.data.tvShow);
     }).catch((error) => reject(error));
 });
 
-const searchShows = () => console.warn('Search not working API broke');
+// const getMostPopular = () => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/shows.json`)
+//     .then((response) => {
+//       resolve(Object.values(response.data));
+//     }).catch((error) => reject(error));
+// });
+
+// const getSingleShow = (id) => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/shows.json?orderBy="id"&equalTo=${id}`)
+//     .then((response) => {
+//       resolve(Object.values(response.data)[0]);
+//     }).catch((error) => reject(error));
+// });
+
+// const searchShows = () => console.warn('Search not working API broke');
 
 export { getMostPopular, searchShows, getSingleShow };
