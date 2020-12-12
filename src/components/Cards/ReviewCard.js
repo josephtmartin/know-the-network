723a1dfd8accfc4b4cbe1e7710fee2fbede01689
josephtmartin <goldglovejo13@gmail.com>
2020-12-12 14:28:08 -0600

    added star ratings to the shows

--- src/components/Cards/ReviewCard.js
+++ src/components/Cards/ReviewCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Ratings from 'react-ratings-declarative';
 
 export default function ReviewCard({ review }) {
   return (
-          <div>
-              <h5>Review: {review.description}</h5>
-              <h5>Rating: {review.rating}</h5>
-          </div>
+    <div>
+        <h5>Review: {review.description}</h5>
+        <Ratings
+          rating={review.rating}
+          widgetDimensions="40px"
+          widgetSpacings="15px"
+        >
+          <Ratings.Widget widgetRatedColor="gold" />
+          <Ratings.Widget widgetRatedColor="gold" />
+          <Ratings.Widget widgetRatedColor="gold" />
+          <Ratings.Widget widgetRatedColor="gold" />
+          <Ratings.Widget widgetRatedColor="gold"/>
+        </Ratings>
+    </div>
   );
 }
