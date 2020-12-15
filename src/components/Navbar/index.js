4b93069bdde01a5f126aa81bd73b057ba0cb6680
josephtmartin <goldglovejo13@gmail.com>
2020-12-14 20:04:12 -0600

    saving

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,40 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavbarText,
+} from 'reactstrap';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import { Link } from 'react-router-dom';
 
-export default class MyNavbar extends Component {
-  logMeOut = (e) => {
+const MyNavbar = ({ user }) => {
+  const logMeOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
-  }
+  };
 
-  render() {
-    const { user } = this.props;
-    return (
-        <div className="MyNavbar">
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand" to='/'>Know The Network</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li className="nav-item">
-                  <Link to='/watchlist' className="nav-link" href="#">Watchlist</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/favorites' className="nav-link" href="#">Favorites</Link>
-                </li>
-              </ul>
-              <div className="form-inline my-2 my-lg-0">
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="lg">
+        <NavbarBrand href="/">Know the Network</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Link to='/watchlist' className="nav-link" href="#">Watchlist</Link>
+            </NavItem>
+            <NavItem>
+              <Link to='/favorites' className="nav-link" href="#">Favorites</Link>
+            </NavItem>
+          </Nav>
+          <NavbarText>
+            <div className="form-inline my-2 my-lg-0">
                 {user && (
-                  <button className="nav-link btn btn-danger" onClick={this.logMeOut}>Logout</button>
+                  <button className="nav-link btn btn-danger" onClick={logMeOut}>Logout</button>
                 )}
-              </div>
             </div>
-          </nav>
-        </div>
-    );
-  }
-}
+          </NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default MyNavbar;
--- src/helpers/data/showData.js
+++ src/helpers/data/showData.js
@@ -1,42 +1,43 @@
 import axios from 'axios';
 
-const baseUrl = 'https://www.episodate.com/api';
+// const baseUrl = 'https://www.episodate.com/api';
 // const baseUrl = 'https://know-the-network-default-rtdb.firebaseio.com';
+const baseUrl
 
-const getMostPopular = () => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/most-popular?page=1`)
-    .then((response) => {
-      resolve(response.data.tv_shows);
-    }).catch((error) => reject(error));
-});
+// const getMostPopular = () => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/most-popular?page=1`)
+//     .then((response) => {
+//       resolve(response.data.tv_shows);
+//     }).catch((error) => reject(error));
+// });
 
-const searchShows = (term) => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/search?q=${term}`)
-    .then((response) => {
-      resolve(response.data.tv_shows);
-    }).catch((error) => reject(error));
-});
+// const searchShows = (term) => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/search?q=${term}`)
+//     .then((response) => {
+//       resolve(response.data.tv_shows);
+//     }).catch((error) => reject(error));
+// });
 
-const getSingleShow = (id) => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/show-details?q=${id}`)
-    .then((response) => {
-      resolve(response.data.tvShow);
-    }).catch((error) => reject(error));
-});
+// const getSingleShow = (id) => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/show-details?q=${id}`)
+//     .then((response) => {
+//       resolve(response.data.tvShow);
+//     }).catch((error) => reject(error));
+// });
 
-const filterMostPopular = (term) => new Promise((resolve, reject) => {
-  axios.get(`${baseUrl}/most-popular?page=1`)
-    .then((response) => {
-      const shows = response.data.tv_shows;
-      const showArray = [];
-      shows.forEach((item) => {
-        if (item.network === term) {
-          showArray.push(item);
-        }
-      });
-      resolve(showArray);
-    }).catch((error) => reject(error));
-});
+// const filterMostPopular = (term) => new Promise((resolve, reject) => {
+//   axios.get(`${baseUrl}/most-popular?page=1`)
+//     .then((response) => {
+//       const shows = response.data.tv_shows;
+//       const showArray = [];
+//       shows.forEach((item) => {
+//         if (item.network === term) {
+//           showArray.push(item);
+//         }
+//       });
+//       resolve(showArray);
+//     }).catch((error) => reject(error));
+// });
 
 // const getMostPopular = () => new Promise((resolve, reject) => {
 //   axios.get(`${baseUrl}/shows.json`)
